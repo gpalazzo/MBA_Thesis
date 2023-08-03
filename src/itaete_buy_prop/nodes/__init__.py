43eb3cf8c4741f208a8e7646383a232e84f3c71f
gpalazzo <gpalazzo93@icloud.com>
2023-08-03 14:43:09 -0300

    add arquivos como backup

--- src/itaete_buy_prop/nodes/__init__.py
+++ src/itaete_buy_prop/nodes/__init__.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from .analise_fin import analise_fin_fte, analise_fin_prm
+from .BKP_fx import fx_fte, fx_prm
+from .BKP_quali_clientes_crm import quali_clientes_crm_fte, quali_clientes_crm_prm
 from .clientes import clientes_prm
 from .funil_vendas import funil_vendas_prm
-from .fx import fx_fte, fx_prm
 from .logreg import logreg_model_fit, logreg_model_predict, logreg_model_relatorio
 from .master_table import (
     cria_master_table,
@@ -11,6 +12,5 @@ from .master_table import (
     mt_seleciona_features,
     mt_split_treino_teste,
 )
-from .quali_clientes_crm import quali_clientes_crm_fte, quali_clientes_crm_prm
 from .spine import spine_labeling, spine_preprocessing, spine_prm
 from .yfinance import yfinance_fte, yfinance_prm, yfinance_raw
